2ea236d18c7c319f0cd2cbceb2dd7f9090bcb9fa
Walter Torricos <walticotc@gmail.com>
2015-07-28 16:03:29 -0400

    error displaying, edit and delete buttons added

--- app/controllers/blogs/add.js
+++ app/controllers/blogs/add.js
@@ -11,17 +11,38 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
     actions: {
         submitBlog: function() {
-
             var title = this.get('title');
             var message = this.get('message');
+            if(!title) {
+                this.set('creationFailed', true);
+                this.set('errorMessage', 'Insert a title please');
+                return;
+            }
+            if(!message) {
+                this.set('creationFailed', true);
+                this.set('errorMessage', 'Insert a message please');
+                return;
+            }
+
             var blog = this.store.createRecord('blog', {
                 title: title,
                 message: message
             });
             var that = this;
-            blog.save().then(function(response) {                
+            blog.save().then(function(response) {
                 that.transitionToRoute('blogs.view', response.id);
+                that.send('resetFields');
             });
+        },
+
+        cancel: function() {
+            this.transitionToRoute('blogs');
+            this.send('resetFields');
+        },
+
+        resetFields: function() {
+            this.set('title', '');
+            this.set('message', '');
         }
     }
 });
--- app/controllers/blogs/view.js
+++ app/controllers/blogs/view.js
@@ -9,4 +9,45 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+    isEditionModeOn: false,
+
+    actions: {
+        edit: function() {
+            this.set('isEditionModeOn', true);
+        },
+
+        cancelEdition: function() {
+            this.set('isEditionModeOn', false);
+        },
+
+        submitEdition: function() {
+            var title = this.get('title');
+            var message = this.get('message');
+            if(!title) {
+                this.set('editionFailed', true);
+                this.set('errorMessage', 'Insert a title please');
+                return;
+            }
+            if(!message) {
+                this.set('editionFailed', true);
+                this.set('errorMessage', 'Insert a message please');
+                return;
+            }
+            this.set('isEditionModeOn', false);
+            alert('not supported yet');
+        },
+
+        confirmDeletion: function() {
+            this.set('needsDeleteConfirmation', true);
+        },
+
+        delete: function() {
+            this.set('needsDeleteConfirmation', false);
+            alert('not supported yet');
+        },
+
+        cancelDeletion: function() {
+            this.set('needsDeleteConfirmation', false);
+        }
+    }
 });
